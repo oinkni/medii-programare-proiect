5f457f5525c5396c45116780fecfb5422bf2c427
oinkni <daniel.georza@yahoo.com>
2025-01-29 13:21:20 +0200

    adaugat filtrare la playeri

--- SportsTournamentApp/Pages/Players/Index.cshtml.cs
+++ SportsTournamentApp/Pages/Players/Index.cshtml.cs
@@ -13,18 +13,37 @@ namespace SportsTournamentApp.Pages.Players
     public class IndexModel : PageModel
     {
         private readonly SportsTournamentApp.Data.SportsTournamentAppContext _context;
+        public string FirstNameFilter { get; set; }
+        public string LastNameFilter { get; set; }
+        public string PositionFilter { get; set; }
 
         public IndexModel(SportsTournamentApp.Data.SportsTournamentAppContext context)
         {
             _context = context;
         }
 
-        public IList<Player> Player { get;set; } = default!;
+        public IEnumerable<Player> Player { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string firstNameSearchString, 
+            string lastNameSearchString, 
+            string positionSearchString)
         {
+            FirstNameFilter = firstNameSearchString;
+            LastNameFilter = lastNameSearchString;
+            PositionFilter = positionSearchString;
+
             Player = await _context.Player
-                .Include(p => p.Team).ToListAsync();
+                .Include(p => p.Team)
+                .ToListAsync();
+
+
+                Player = _context.Player.Where(s =>
+                    (String.IsNullOrEmpty(FirstNameFilter) || s.FirstName.Contains(FirstNameFilter))
+                    &&
+                    (String.IsNullOrEmpty(LastNameFilter) || s.LastName.Contains(LastNameFilter))
+                    &&
+                    (String.IsNullOrEmpty(PositionFilter) || s.Position.Contains(PositionFilter))
+                );
         }
     }
 }
